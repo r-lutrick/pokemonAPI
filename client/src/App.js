533299de81a047d53cf72a0005fcc4a55b282574
Robert Lutrick <r.lutrick@icloud.com>
2023-01-17 18:24:45 -0600

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FetchAPI } from './components/FetchAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <FetchAPI />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/FetchAPI.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+
+export const FetchAPI = () => {
+    const [pokemon, setPokemon] = useState([])
+
+    const handleFetch = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=9")
+            .then((res) => {
+                return res.json()
+            })
+            .then((res) => {
+                setPokemon(res.results)
+                console.log(pokemon)
+            }
+            )
+            .catch((error) =>
+                console.log("error message:", error)
+            )
+    }
+
+    return (
+        <div className='container w-25'>
+            <h1>Fetch API</h1>
+            <button className='btn btn-dark my-3' onClick={handleFetch}>Click Me</button>
+            <ul className='list-group'>
+                {
+                    pokemon.map((poke, i) => {
+                        return (
+                            <li className='list-group-item' key={i}>{poke.name}</li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
